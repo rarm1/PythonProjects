d925115ac6426757e32596e19a3aa68f49280fee
RArm1 <richard.armstrong@margetts.com>
2023-06-05 14:16:02 +0100

    Restructuring AOL file for greater visibility and easier clearing. TODO for the project remains to be getting it able to run via Batch.

---
+++ The Harpoon/Harpoon.bat
@@ -0,0 +1,5 @@
+x:
+cd X:\Fund Management\Fund Management Team Files\FM Personal Folders\Richard\PycharmProjects\The Harpoon
+set VENV_PATH=X:\Fund Management\Fund Management Team Files\FM Personal Folders\Richard\PycharmProjects\venv
+set PATH=%PTHON_PTH%;%PATH%
+python main_harpoon.py
